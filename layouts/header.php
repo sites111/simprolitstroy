dc71ea8cab5b7860193487326205ac333f2309ac
sites111 <106382892+sites111@users.noreply.github.com>
2022-06-08 05:02:08 +0300

    вынес менюшку в файл

---
+++ layouts/header.php
@@ -0,0 +1,273 @@
+<div class="  ">
+    <nav class="navbar navbar-expand-lg navbar-light w-100 h-100 " style=" padding-top: 0 !important;">
+     
+      <div class="container w-100 ">
+        <a class="navbar-brand text-light d-sm-none d-lg-block d-xl-none  d-none d-sm-block " href="#">
+          <img class="h-100 " src="images/Group 1.svg " style="padding-top: 0 !important;">
+         </a>
+        <div class="row gx-1 w-100 ">
+          <div class="col-6 col-sm-3  col-md-2 col-lg-2 col-xl-4 col-xxl-4  d-lg-none d-xl-block ">
+            <div class="row gx-lg-0 gx-xl-1 ">
+              <div class="col-auto">
+                <a class="navbar-brand text-light" href="#">
+                  <img class="" src="images/Group 1.svg"  alt="">
+                 </a>
+              </div>
+              
+              <div class=" col-xl-7 col-xxl-6 mt-3 gx-md-1 d-sm-none d-md-block d-lg-none d-xl-block d-md-none d-none d-sm-block ">
+                <div class="pt-2">
+                  <span class="logo-text " style="    font-size: 15px !important;font-weight: 700;">
+                    Строим современные 
+                  </span>
+                  <br>
+                  <span class="logomintext">
+                    дома в Казани и  республике
+                </span>  
+                </div>
+               
+            </div>
+
+            </div>
+            
+          </div>
+      
+
+
+          
+
+          <div class="d-none d-sm-block col-sm-4 col-md-5 col-lg-4  col-xl-2 col-xxl-2 mt-4 mt-lg-1 mt-xl-4 ">
+            <div class="row pt-1 gx-lg-3 gx-xl-2">
+              <div class="col-auto">
+                <img class="img-fluid" src="https://static.tildacdn.info/tild3930-3134-4131-b633-643361653562/fa-solid_map-marker-.svg" alt="">
+              </div>
+
+              <div class="col">
+                <span class="text-light">г. Казань  <br>
+                  ул. Ташаяк д. 2А</span>
+              </div>
+            </div>
+            
+          </div>
+
+          <div class="d-none d-sm-block d-sm-none d-md-block d-md-none d-lg-block col-lg-3 col-xl-2 col-xxl-2 mt-4 mt-lg-1 mt-xl-4 ">
+            <div class="row pt-1 gx-lg-3 gx-xl-2">
+              <div class="col-auto">
+                <img class="img-fluid"  src="https://static.tildacdn.info/tild3634-3566-4663-b763-323630373161/Group_27.svg" alt="">
+              </div>
+              <div class="col-2">
+                <span class="text-light border-bottom border-1 border-success " style="border-color: #8ED834 !important;">Калькулятор <br> стоимости</span>
+              </div>
+            </div>
+            
+           
+          </div>
+          <div class="d-none d-sm-block d-sm-none d-md-block col-lg-2 col-xl-2 col-xxl-2 mt-4 text-center mt-lg-1 mt-xl-4 d-md-none d-lg-block ">
+            <img class="pt-3" src="https://static.tildacdn.info/tild3065-6531-4066-b633-353563666664/logos_whatsapp.svg" alt="">
+            <img class="pt-3"  src="https://static.tildacdn.info/tild6563-3833-4035-a439-633435626437/vk.svg" alt="">
+            <img class="pt-3" src="https://static.tildacdn.info/tild6166-3164-4234-b732-343530666666/logos_telegram.svg" alt="">
+          </div>
+
+          <div class="col-6 col-sm-5 col-md-5 col-lg-3 col-xl-2 col-xxl-2 mt-4 text-end mt-lg-2 mt-xl-4 ">
+            <div class="pt-2">
+              <span class=" text-light" style="font-family: 'Montserrat';
+              font-style: normal;
+              font-weight: 600;
+              font-size: 20px;
+              line-height: 24px;
+              
+              color: #FFFFFF;">
+                +7 (843) 212 61-77
+              </span>
+              <br>
+              <div class="spinner-grow btn-successs "  role="status" style="max-width: 14px; max-height: 14px; margin-top: 0px; ">
+              </div>
+              <span class="border-bottom border-1 border-success " style="font-family: 'Montserrat';
+              font-style: normal;
+              font-weight: 500;
+              font-size: 14px;
+              line-height: 14px;
+              padding-top: 30px !important;
+              border-color: #8ED834 !important;
+              color: #8ED834;
+              "> Звоните, мы на связи</span>
+            </div>
+            </div>
+          
+        </div> 
+       
+      </div>
+
+      
+    </nav>
+    <!-- мобильное -->
+    <div class="container   d-none d-sm-block mt-4 pb-4 " style="z-index: 1000 !important">
+      <ul class="nav nav-fill justify-content-center" style="display: flex !important;">
+          <li class="nav-item">
+            <a class="nav-link active text-light menu-up" href="https://simprolitstroy.ru/#rec448276217">ПРОЕКТЫ ДОМОВ</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-light menu-up dropdown-toggle" id="navadv" role="button" data-toggle="dropdown" aria-expanded="false" href="#">УСЛУГИ</a>
+            <ul class="dropdown-menu dropdown-menu-dark w-100 mt-2 p-0" aria-labelledby="navadv" style="background-color: rgba(52, 58, 64, 0.98); max-width: 230px !important;">
+              <div class="row text-center p-3 ">
+                  <div class="col">
+                      <li onclick="modalSow('Интересует строительство под ключ?');" style="cursor: pointer; text-align: left; color: #fff;">Строительство под ключ</li>
+                      <li onclick="modalSow('Интересует утепление крыш?');" style="cursor: pointer; text-align: left; color: #fff;">Утепление крыш</li>
+                      <li onclick="modalSow('Интересует утепление полов?');" style="cursor: pointer; text-align: left; color: #fff;">Утепление полов</li>
+                      <li onclick="modalSow('Интересует утепление стен?');" style="cursor: pointer; text-align: left; color: #fff;">Утепление стен</li>
+                  </div>
+              </div>
+          </ul>
+          </li>
+         
+          <li class="nav-item">
+            <a class="nav-link text-light  menu-up  dropdown-toggle" href="#" id="navstroymat" role="button" data-toggle="dropdown" aria-expanded="false">СТРОЙМАТЕРИАЛЫ</a>
+            <ul class="dropdown-menu dropdown-menu-dark w-100 mt-2 p-0" aria-labelledby="navstroymat" style="max-width:260px !important; background-color: rgb(52 58 64 / 98%);">
+              <div class="row text-center p-3 ">
+                  <div class="col">
+                      <li onclick="modalSow('Интересуют пенобетонные блоки?');" style="cursor: pointer; text-align: left; color: #fff;">Пенобетонные блоки</li>
+                      <li onclick="modalSow('Интересуют полистеролбетонные блоки?');" style="cursor: pointer; text-align: left; color: #fff;">Полистеролбетонные блоки</li> 
+                  </div>
+              </div>
+          </ul>
+          </li>
+          
+          <li class="nav-item">
+            <a class="nav-link text-light menu-up" href="https://simprolitstroy.ru/#">ДОКУМЕНТАЦИЯ</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-light menu-up" href="https://simprolitstroy.ru/#rec448407412">ОТЗЫВЫ</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-light menu-up" href="#rec448425078">КОНТАКТЫ</a>
+          </li>
+       
+        </ul>
+      
+  </div>
+    
+    <div class="container mt-2 pb-2 d-block d-sm-none">
+      <div class="row">
+        <div class="col text-light">
+          <button type="button" class=" text-light border-0 bg-none" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            <span class="h2">
+              МЕНЮ
+            </span>
+          </button>
+          
+        </div>
+        <div class="col text-light d-flex justify-content-end">
+          <button type="button" class=" text-light border-0 bg-none" data-bs-toggle="modal" data-bs-target="#exampleModal2">
+            <span class="h2">
+              УСЛУГИ
+            </span>
+          </button>
+        </div>
+      </div>
+    </div>
+    <!-- Button trigger modal -->
+
+
+<!-- Modal -->
+<div class="modal  fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-fullscreen w-75 ">
+    <div class="modal-content bg-dark text-light">
+      <div class="modal-header border-0">
+        <h5 class="modal-title" id="exampleModalLabel">
+          <img class="" src="fonts/ant-design_home-outl.svg" alt="">
+        </h5>
+        <button type="button" class="btn-close text-light btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <div class="row gy-3">
+          <div class="col-12">
+            <span class="h5 ">ПРОЕКТЫ ДОМОВ</span>
+            <li class=" nav-link text-light"><a class=" border-bottom border-1 border-light text-light text-decoration-none" href="">Кирпичные дома</a> </li>
+
+            <li class=" nav-link text-light"><a class=" border-bottom border-1 border-light text-light text-decoration-none" href="">Кирпичные дома</a> </li>
+          </div>
+          <div class="col-12">
+            <span class="h5">ПРОЕКТЫ ДОМОВ</span>
+          </div>
+          <div class="col-12">
+            <span class="h5">ПРОЕКТЫ ДОМОВ</span>
+          </div>
+          <div class="col-12">
+            <span class="h5">ПРОЕКТЫ ДОМОВ</span>
+          </div>
+          <div class="col-12">
+            <span class="h5">ПРОЕКТЫ ДОМОВ</span>
+          </div>
+        </div>
+        
+        
+      </div>
+      <div class="modal-footer border-0 display-flex justify-content-start">
+        <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="30px" height="30px" viewBox="0 0 99 99" enable-background="new 0 0 99 99" space="preserve">
+          <path style="fill:#ffffff;" fill-rule="evenodd" clip-rule="evenodd" d="M49 98C76.062 98 98 76.062 98 49C98 21.938 76.062 0 49 0C21.938 0 0 21.938 0 49C0 76.062 21.938 98 49 98ZM22 34C22.4442 55.3198 33.1042 68.1323 51.7933 68.1323H52.8527V55.9351C59.7202 56.6182 64.9132 61.6409 66.9973 68.1323H76.7008C74.0359 58.4292 67.0314 53.0649 62.658 51.0149C67.0314 48.4866 73.1814 42.3367 74.6505 34H65.8353C63.922 40.7649 58.251 46.915 52.8527 47.4958V34H44.0374V57.6433C38.5707 56.2766 31.6692 49.6482 31.3617 34H22Z"></path>
+        </svg>
+        <svg class="t-sociallinks__svg" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink" width="30px" height="30px" viewBox="0 0 60 60" space="preserve">
+          <desc>Telegram</desc>
+          <path style="fill:#ffffff;" d="M30 0C13.4 0 0 13.4 0 30s13.4 30 30 30 30-13.4 30-30S46.6 0 30 0zm16.9 13.9l-6.7 31.5c-.1.6-.8.9-1.4.6l-10.3-6.9-5.5 5.2c-.5.4-1.2.2-1.4-.4L18 32.7l-9.5-3.9c-.7-.3-.7-1.5 0-1.8l37.1-14.1c.7-.2 1.4.3 1.3 1z"></path>
+          <path style="fill:#ffffff;" d="M22.7 40.6l.6-5.8 16.8-16.3-20.2 13.3"></path>
+      </svg>
+      <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="30px" height="30px" viewBox="0 0 99 99" style="enable-background:new 0 0 99 99;" space="preserve">
+        <path style="fill:#ffffff;" d="M63.892 53.114C63.143 52.739 59.462 50.928 58.775 50.678C58.089 50.428 57.589 50.303 57.09 51.053C56.591 51.803 55.156 53.489 54.719 53.989C54.282 54.489 53.845 54.551 53.097 54.176C52.348 53.801 49.935 53.011 47.075 50.459C44.849 48.473 43.346 46.021 42.909 45.272C42.472 44.522 42.862 44.117 43.237 43.744C43.574 43.408 43.986 42.869 44.36 42.432C44.734 41.995 44.859 41.682 45.109 41.183C45.359 40.683 45.234 40.246 45.047 39.871C44.86 39.496 43.362 35.81 42.738 34.311C42.13 32.851 41.513 33.049 41.053 33.026C40.617 33.004 40.117 33 39.618 33C39.119 33 38.308 33.187 37.621 33.937C36.935 34.687 35 36.498 35 40.184C35 43.87 37.683 47.431 38.058 47.93C38.432 48.43 43.338 55.993 50.85 59.237C52.637 60.009 54.031 60.469 55.119 60.814C56.913 61.384 58.545 61.304 59.836 61.111C61.275 60.896 64.266 59.3 64.89 57.551C65.514 55.802 65.514 54.302 65.327 53.99C65.14 53.677 64.641 53.489 63.892 53.114Z"></path>
+        <path style="fill:#ffffff;" fill-rule="evenodd" clip-rule="evenodd" d="M49 98C76.062 98 98 76.062 98 49C98 21.938 76.062 0 49 0C21.938 0 0 21.938 0 49C0 76.062 21.938 98 49 98ZM50.221 17C58.238 17.003 65.763 20.124 71.421 25.789C77.08 31.454 80.194 38.983 80.189 46.992C80.182 63.515 66.737 76.96 50.22 76.96H50.208C45.193 76.958 40.264 75.7 35.887 73.312L20 77.479L24.252 61.949C21.63 57.405 20.25 52.249 20.252 46.967C20.258 30.443 33.703 17 50.221 17Z"></path>
+      </svg>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<!-- Modal -->
+<div class="modal  fade" id="exampleModal2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-fullscreen w-75 ">
+    <div class="modal-content bg-dark text-light">
+      <div class="modal-header border-0">
+        <h5 class="modal-title" id="exampleModalLabel">
+          <img class="" src="fonts/ant-design_home-outl.svg" alt="">
+        </h5>
+        <button type="button" class="btn-close text-light btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <div class="row gy-3">
+          <div class="col-12">
+            <span class="h5">УСЛУГИ ДОМОВ</span>
+          </div>
+          <div class="col-12">
+            <span class="h5">УСЛУГИ ДОМОВ</span>
+          </div>
+          <div class="col-12">
+            <span class="h5">УСЛУГИ ДОМОВ</span>
+          </div>
+          <div class="col-12">
+            <span class="h5">УСЛУГИ ДОМОВ</span>
+          </div>
+          <div class="col-12">
+            <span class="h5">УСЛУГИ ДОМОВ</span>
+          </div>
+          
+        </div>
+        
+        
+      </div>
+      <div class="modal-footer border-0 display-flex justify-content-start">
+        <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="30px" height="30px" viewBox="0 0 99 99" enable-background="new 0 0 99 99" space="preserve">
+          <path style="fill:#ffffff;" fill-rule="evenodd" clip-rule="evenodd" d="M49 98C76.062 98 98 76.062 98 49C98 21.938 76.062 0 49 0C21.938 0 0 21.938 0 49C0 76.062 21.938 98 49 98ZM22 34C22.4442 55.3198 33.1042 68.1323 51.7933 68.1323H52.8527V55.9351C59.7202 56.6182 64.9132 61.6409 66.9973 68.1323H76.7008C74.0359 58.4292 67.0314 53.0649 62.658 51.0149C67.0314 48.4866 73.1814 42.3367 74.6505 34H65.8353C63.922 40.7649 58.251 46.915 52.8527 47.4958V34H44.0374V57.6433C38.5707 56.2766 31.6692 49.6482 31.3617 34H22Z"></path>
+        </svg>
+        <svg class="t-sociallinks__svg" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink" width="30px" height="30px" viewBox="0 0 60 60" space="preserve">
+          <desc>Telegram</desc>
+          <path style="fill:#ffffff;" d="M30 0C13.4 0 0 13.4 0 30s13.4 30 30 30 30-13.4 30-30S46.6 0 30 0zm16.9 13.9l-6.7 31.5c-.1.6-.8.9-1.4.6l-10.3-6.9-5.5 5.2c-.5.4-1.2.2-1.4-.4L18 32.7l-9.5-3.9c-.7-.3-.7-1.5 0-1.8l37.1-14.1c.7-.2 1.4.3 1.3 1z"></path>
+          <path style="fill:#ffffff;" d="M22.7 40.6l.6-5.8 16.8-16.3-20.2 13.3"></path>
+      </svg>
+      <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="30px" height="30px" viewBox="0 0 99 99" style="enable-background:new 0 0 99 99;" space="preserve">
+        <path style="fill:#ffffff;" d="M63.892 53.114C63.143 52.739 59.462 50.928 58.775 50.678C58.089 50.428 57.589 50.303 57.09 51.053C56.591 51.803 55.156 53.489 54.719 53.989C54.282 54.489 53.845 54.551 53.097 54.176C52.348 53.801 49.935 53.011 47.075 50.459C44.849 48.473 43.346 46.021 42.909 45.272C42.472 44.522 42.862 44.117 43.237 43.744C43.574 43.408 43.986 42.869 44.36 42.432C44.734 41.995 44.859 41.682 45.109 41.183C45.359 40.683 45.234 40.246 45.047 39.871C44.86 39.496 43.362 35.81 42.738 34.311C42.13 32.851 41.513 33.049 41.053 33.026C40.617 33.004 40.117 33 39.618 33C39.119 33 38.308 33.187 37.621 33.937C36.935 34.687 35 36.498 35 40.184C35 43.87 37.683 47.431 38.058 47.93C38.432 48.43 43.338 55.993 50.85 59.237C52.637 60.009 54.031 60.469 55.119 60.814C56.913 61.384 58.545 61.304 59.836 61.111C61.275 60.896 64.266 59.3 64.89 57.551C65.514 55.802 65.514 54.302 65.327 53.99C65.14 53.677 64.641 53.489 63.892 53.114Z"></path>
+        <path style="fill:#ffffff;" fill-rule="evenodd" clip-rule="evenodd" d="M49 98C76.062 98 98 76.062 98 49C98 21.938 76.062 0 49 0C21.938 0 0 21.938 0 49C0 76.062 21.938 98 49 98ZM50.221 17C58.238 17.003 65.763 20.124 71.421 25.789C77.08 31.454 80.194 38.983 80.189 46.992C80.182 63.515 66.737 76.96 50.22 76.96H50.208C45.193 76.958 40.264 75.7 35.887 73.312L20 77.479L24.252 61.949C21.63 57.405 20.25 52.249 20.252 46.967C20.258 30.443 33.703 17 50.221 17Z"></path>
+      </svg>
+      </div>
+    </div>
+  </div>
+</div>
+    
+  </div>
