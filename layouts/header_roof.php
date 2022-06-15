b9acf68b5baf2676d21cdea38ae9c0d103c8749f
Maxim Churyanov <maxim12521@gmail.com>
2022-06-15 16:12:16 +0300

    фикс багов

--- layouts/header_roof.php
+++ layouts/header_roof.php
@@ -1,46 +1,17 @@
 <div class="header bg-dark" style="background-image: url('../images/bg-roof.png')  ">
   
   <?php
-  include '../layouts/header.php';
+  include 'header.php';
   ?>
 
   <?php
-  include '../layouts/menu_desctop.php';
+  include 'menu_desctop.php';
   ?>
+ 
+   <?php
+    include 'menu_mobile.php';
+    ?>
 
-  <!-- мобильное -->
-  <div class="container mt-2 pb-2 d-block d-sm-none">
-    <div class="row">
-      <div class="col text-light">
-        <button type="button" class="text-light border-0 bg-none" data-bs-toggle="modal" data-bs-target="#exampleModal">
-          <div class="row g-2">
-            <div class="col">
-              <span class="h2">
-                МЕНЮ
-              </span>
-            </div>
-            <div class="col-auto">
-              <img class="pt-1" style="max-width: 25px; transform: rotate (180deg) !important;" src="../ant-design_lerft-outl1.svg" alt="" />
-            </div>
-          </div>
-        </button>
-      </div>
-      <div class="col text-light d-flex justify-content-end">
-        <button type="button" class="text-light border-0 bg-none" data-bs-toggle="modal" data-bs-target="#exampleModal2">
-          <div class="row g-2">
-            <div class="col-auto">
-              <img class="pt-1" style="max-width: 25px" src="../ant-design_left-outl.svg" alt="" />
-            </div>
-            <div class="col">
-              <span class="h2">
-                УСЛУГИ
-              </span>
-            </div>
-          </div>
-        </button>
-      </div>
-    </div>
-  </div>
   <!-- Button trigger modal -->
   <div class="container mt-4 pb-4 d-none d-sm-block" style="z-index: 1000 !important; color: #fff;">
     <span style="    color: #ffffff; font-size: 45px; font-family: 'Montserrat', Arial, sans-serif; line-height: 1.55;font-weight: 400;">
@@ -210,9 +181,5 @@
         </div>
       </div>
     </div>
-  </div>
-  <!-- Modal -->
-  <?php
-  include '../layouts/menu_mobile.php';
-  ?>
+  </div> 
 </div>
